7fb49f9e71c531c38cb391d194df650e2327de0a
Daniel Golembiewski <dgolembiewski@falconlogic.com>
2018-08-01 11:27:39 -0400

    Initial

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,42 @@
-import { Component } from '@angular/core';
+import { Component, OnInit, ViewChild } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { AgGridNg2 } from 'ag-grid-angular';
 
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+    selector: 'app-root',
+    templateUrl: './app.component.html',
+    styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
-  title = 'mygrid';
+export class AppComponent implements OnInit {
+    @ViewChild('agGrid') agGrid: AgGridNg2;
+  
+    title = 'Comments Testing From An External API';
+
+    columnDefs = [
+        {headerName: 'Post ID', field: 'postId', checkboxSelection: true },
+        {headerName: 'ID', field: 'id' },
+        {headerName: 'User', field: 'name'},
+        {headerName: 'Email', field: 'email'},
+        {headerName: 'Comments', field: 'body'},        
+    ];
+
+    rowData: any;
+
+
+    constructor(private http: HttpClient) {
+    
+    }
+    
+    ngOnInit() {
+         this.rowData = this.http.get('https://jsonplaceholder.typicode.com/comments');
+    }
+
+    getSelectedRows() {
+      const selectedNodes = this.agGrid.api.getSelectedNodes();
+      const selectedData = selectedNodes.map( node => node.data );
+      const selectedDataStringPresentation = selectedData.map( node => node.name + ' ' + node.email).join(', ');
+      alert(`Selected nodes: ${selectedDataStringPresentation}`);
+  }
+
+   
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,13 +2,28 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { AgGridModule } from 'ag-grid-angular';
+import { HttpClientModule } from '@angular/common/http';
+
+import { CommonModule } from '@angular/common';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+ 
+import { ToastrModule } from 'ngx-toastr';
+
+
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    // use ng components for cells/headers
+    AgGridModule.withComponents([]),
+    HttpClientModule,
+    CommonModule,
+    BrowserAnimationsModule, // required animations module
+    ToastrModule.forRoot() // ToastrModule added
   ],
   providers: [],
   bootstrap: [AppComponent]
